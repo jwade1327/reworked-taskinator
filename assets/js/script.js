559be2fe756a9206f85c7fc0a99668cff9f9f73f
Jessica Wade <jessica.wade@parameter1.com>
2021-06-28 09:41:08 -0500

    drag styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -194,6 +194,7 @@ var dropZoneDragHandler = function(event) {
   var taskListEl = event.target.closest(".task-list");
   if (taskListEl) {
     event.preventDefault();
+    taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
   }
 };
 
@@ -215,8 +216,16 @@ var dropTaskHandler = function(event) {
     statusSelectEl.selectedIndex = 2;
   }
 
+  dropZoneEl.removeAttribute("style");
   dropZoneEl.appendChild(draggableElement);
 };
+
+var dragLeaveHandler = function(event) {
+  var taskListEl = event.target.closest(".task-list");
+  if (taskListEl) {
+    taskListEl.removeAttribute("style");
+  }
+}
 
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
@@ -230,3 +239,4 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
